b4834a8b783783253e5229d8c9c00c43b20b5f0b
Ben Walters <ben.walters@me.com>
2025-09-29 15:45:52 +0100

    Remove pino pretty dependency

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -1,29 +1,10 @@
 import pino, { Logger } from 'pino';
 
-let logger: Logger;
-
-const isLocal = process.env.IS_LOCAL === 'true';
-
-const base = {
-  name: 'events-engine',
-};
-
-if (isLocal) {
-  logger = pino({
-    base,
-    level: process.env.LOG_LEVEL || 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-      },
-    },
-  });
-} else {
-  logger = pino({
-    base,
-    level: process.env.LOG_LEVEL || 'info',
-  });
-}
+const logger: Logger = pino({
+  base: {
+    name: 'WRABBER',
+  },
+  level: process.env.LOG_LEVEL || 'info',
+});
 
 export { logger };
